61c925f47657ede90c63ba53d60e11011863ed84
Ryan C <47868257+rycl1@users.noreply.github.com>
2022-10-04 23:44:15 -0600

    Update next.sol

--- next.sol
+++ next.sol
@@ -10,44 +10,40 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumerV3 {
 
-    struct user {
-        address user_address;
+    /*struct user {
+        address user_address1;
+        address user_address2;
         uint amount;
-        uint connect_val;
         // If we use this connect_val to connect, we have to make sure there's no latency between two
         // initializers, so that nobody jumps in between. Just have it initialize at the same time if possible?
         // Going to need a mapping to look ppl up by their struct (maybe)
-      }
+      } */
 
     AggregatorV3Interface internal priceFeed;
+    address public user_deployer;
+    address public user2;
+    uint public full_val;
+    uint public tok_val;
+    uint public datetime;
 
     /**
      * Network: Goerli
      * Aggregator: ETH/USD
      * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
      */
-    constructor() {
+
+    
+    constructor(address user2, uint full_val, uint tok_val, uint datetime) {
         priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        user_deployer = msg.sender;
+        user2 = user2;
+        full_val = full_val;
+        tok_val = tok_val;
+        datetime = datetime;
     }
-
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int) {
-        (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-        return price;
-    }
-
-    function test() public view returns (string memory) {
-        string memory p1 = "P1";
-        string memory p2 = "P2";
-
+    
+    //upkeep function is triggered by Chainlink timekeeper on designated datetime
+    function upkeep() public returns (address) {
         (
             /*uint80 roundID*/,
             int price,
@@ -56,13 +52,15 @@ contract PriceConsumerV3 {
             /*uint80 answeredInRound*/
         ) = priceFeed.latestRoundData();
 
-        if (price > 133466002243) {
-            return p1;
+        //133466002243
+
+
+        if (price > int(tok_val)) {
+            return user_deployer;
         } else {
-            return p2;
+            return user2;
         }
 
+    }  
 
-
-    }
 }
