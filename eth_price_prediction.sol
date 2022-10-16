56b2062d7e4691d68d00c6a3da6f5592df74b9af
Ryan C <47868257+rycl1@users.noreply.github.com>
2022-10-15 23:54:49 -0600

    Update eth_price_prediction.sol

--- eth_price_prediction.sol
+++ eth_price_prediction.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Distribute {
+contract TwoPartyPricePrediction {
 
     /*struct user {
         address user_address1;
