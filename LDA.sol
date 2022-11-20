7c206d78c00b1269dd332c5ab77561030ca803c9
Ryan C <47868257+rycl1@users.noreply.github.com>
2022-11-19 18:46:25 -0700

    Update LDA.sol

--- LDA.sol
+++ LDA.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.0;
 
 contract LDA { 
 
+  function getBalance () external view returns(uint) {
+        return address(this).balance;
 
-
+      }
 
   function transferbal() public payable {
-    //payable(address1).transfer(wager1 * 3 / 2);
+    //payable(address).transfer(value);
     }
 
 }
