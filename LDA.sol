8c1ccd51b5762281c1daac7d2b44e52547610ecd
Ryan C <47868257+rycl1@users.noreply.github.com>
2022-11-19 18:45:10 -0700

    Update LDA.sol

--- LDA.sol
+++ LDA.sol
@@ -1,1 +1,15 @@
 
+pragma solidity ^0.8.0; 
+
+import "./safemath.sol";
+
+contract LDA { 
+
+
+
+
+  function transferbal() public payable {
+    //payable(address1).transfer(wager1 * 3 / 2);
+    }
+
+}
